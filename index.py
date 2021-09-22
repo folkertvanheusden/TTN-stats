7318c47873b17e30f6bf7def9cfa3e4d678a40f4
Folkert van Heusden <folkert@vanheusden.com>
2021-09-22 21:26:55 +0200

    scaling fix & fix for air time calculation

--- index.py
+++ index.py
@@ -405,12 +405,22 @@ print('</div>')
 print('<div class="container">')
 print('<h2 id="at">air time per hour</h2>')
 
-c.execute('select h, avg(ms_airtime) as avg_ma, min(ms_airtime) as minimum, max(ms_airtime) as maximum from (select hour(time) as h, sum(size * 8 / bits_s * 1000) as ms_airtime from rxpk, (select bits_s from rxpk, data_rates where datr_str=rxpk.datr) as rate group by date(time), hour(time)) as bla group by h')
+c.execute('select h, avg(ms_airtime) as avg_ma, min(ms_airtime) as minimum, max(ms_airtime) as maximum from (select hour(time) as h, sum(size * 8 / bits_s) as ms_airtime from rxpk, (select bits_s from rxpk, data_rates where datr_str=rxpk.datr) as rate group by date(time), hour(time)) as bla group by h')
 
 print('<table><tr><th>hour</th><th>minimum</th><th>maximum</th><th>avg % of hour</th><th>avg total</th><th></th></tr>')
 
+data = []
+max_avg = -1
+
 for (h, avg_ma, minimum, maximum) in c:
-    stars = '&#9619;' * int(30 * (avg_ma / 3600000))
+    data.append((h, avg_ma, minimum, maximum))
+
+    if avg_ma > max_avg:
+        max_avg = avg_ma
+
+for (h, avg_ma, minimum, maximum) in data:
+
+    stars = '&#9619;' * int(30 * (avg_ma / max_avg))
 
     print(f'<tr><td>{h}</td><td>{minimum:.2f}ms</td><td>{maximum:.2f}ms</td><td>{avg_ma * 100 / 3600000:.2f}%</td><td>{avg_ma:.2f}ms</td><td>{stars}</tr>')
 
